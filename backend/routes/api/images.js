9a4129a2c25f449d70f88ac43e8d0c001abb2fe2
Karandeep <jinnie1996@hotmail.com>
2022-01-10 23:37:33 -0800

    working on api routes for images

---
+++ backend/routes/api/images.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+
+const { Image } = require('../../db/models');
+const router = require('./session');
+
+
+
+module.exports = router
