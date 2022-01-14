01a2fa36cdba6a7984ece6dc606728b254a0776e
Karandeep <jinnie1996@hotmail.com>
2022-01-14 01:45:33 -0800

    changed start button

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -1,5 +1,5 @@
 import './Homepage.css'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link, NavLink } from 'react-router-dom'
 function Homepage() {
 
     const history = useHistory()
@@ -13,7 +13,7 @@ function Homepage() {
             <h1 className="inspiration">Find your inspiration.</h1>
             <h2 className="community">Join the Crittr community, home to all pet owners and animal lovers!
             </h2>
-            <button className="startbutton"> Start for free</button>
+                <div className="startbutton"> Start for free</div>
             </main>
         </div>
     )
